cf2e1b3932952ed438d8bbe26b1e7a1c3b5fc35d
Doug Proctor <doug-proctor@users.noreply.github.com>
2020-06-21 22:57:58 +0100

    Added a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function PageNotFound() {
+  return (
+    <div style={{ textAlign: "center", marginTop: 150 }}>
+      <h1>4😬4</h1>
+      <p style={{ marginTop: 10 }}>We can't find that page</p>
+    </div>
+  )
+}
