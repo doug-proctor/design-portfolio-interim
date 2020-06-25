6a2232c833b1f4911a373a5354ef039dc4ab14ab
Doug Proctor <doug-proctor@users.noreply.github.com>
2020-06-25 16:42:22 +0100

    Fixes; added CV

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,11 @@ import styles from "./Footer.module.css"
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      {/*<div className={styles.line}>👋</div>*/}
-      <div className={styles.line}>07482178555</div>
-      <div className={styles.line}>hello@dougproctor.co.uk</div>
+      <div className={styles.line}>👋</div>
+      <div className={styles.line}>07482178555 / hello@dougproctor.co.uk</div>
+      <div className={styles.line}>
+        <a href="/Doug-Proctor-CV-Lead-UX-UI.pdf">Curriculum vitae</a>
+      </div>
     </footer>
   )
 }
